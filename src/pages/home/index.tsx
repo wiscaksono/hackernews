5dfb9185633d04b1c5dd2ac03707a37f7969796b
Wisnu Wicaksono <63142229+wiscaksono@users.noreply.github.com>
2023-06-24 20:16:11 +0700

    fix: life cycle component

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
-import { Article } from "../../components";
+import { Article, ArticleLoader } from "../../components";
 import { getStoryIds } from "../../services";
 import { useInfiniteScroll } from "../../hooks";
 import { NewStoriesResponse, IdParams } from "../../types";
@@ -12,17 +12,35 @@ export default function Home() {
   const [storyIds, setStoryIds] = useState<NewStoriesResponse>();
 
   useEffect(() => {
-    (async () => {
-      const storyIds = await getStoryIds((topics as IdParams) || "newstories");
-      setStoryIds(storyIds);
-    })();
+    let isMounted = true;
+
+    const fetchStoryIds = async () => {
+      const fetchedStoryIds = await getStoryIds(
+        (topics as IdParams) || "newstories"
+      );
+      if (isMounted) {
+        setStoryIds(fetchedStoryIds);
+      }
+    };
+
+    fetchStoryIds();
+
+    return () => {
+      isMounted = false;
+    };
+  }, [topics]);
+
+  useEffect(() => {
+    setStoryIds(undefined);
   }, [topics]);
 
   return (
     <section className="flex items-center justify-center flex-col space-y-4 my-4 divide-y">
-      {storyIds?.slice(0, count).map((storyId, i) => (
-        <Article key={i} storyId={storyId} idx={i} />
-      ))}
+      {storyIds
+        ? storyIds
+            .slice(0, count)
+            .map((storyId, i) => <Article key={i} storyId={storyId} idx={i} />)
+        : [...Array(count)].map((_, i) => <ArticleLoader key={i} idx={i} />)}
     </section>
   );
 }
