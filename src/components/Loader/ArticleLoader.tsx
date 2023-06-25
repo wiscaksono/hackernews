dd5865021d1bb5f637b3e859ce42d6cf596be36e
Wisnu Wicaksono <63142229+wiscaksono@users.noreply.github.com>
2023-06-25 13:55:07 +0700

    feat: randomize loader width

--- src/components/Loader/ArticleLoader.tsx
+++ src/components/Loader/ArticleLoader.tsx
@@ -1,14 +1,18 @@
 export const ArticleLoader = ({ idx }: { idx: number }) => {
+  const randomWidth = `${Math.floor(Math.random() * 91) + 10}%`;
   return (
     <article className="space-y-2 w-full py-2">
       <div className="space-y-1">
         <div
-          className="w-full h-4 bg-gray-300 animate-pulse"
-          style={{ animationDelay: `1.${idx}s` }}
+          className="h-4 bg-gray-300 animate-pulse"
+          style={{ animationDelay: `1.${idx}s`, width: randomWidth }}
         />
       </div>
       <div className="flex items-center justify-between">
-        <div className="w-20 h-3 bg-gray-300" />
+        <div className="flex items-center gap-x-2">
+          <div className="w-20 h-3 bg-gray-300" />
+          <div className="w-20 h-3 bg-gray-300" />
+        </div>
         <div className="w-28 h-3 bg-gray-300" />
       </div>
     </article>
